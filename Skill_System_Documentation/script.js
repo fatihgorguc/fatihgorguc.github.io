e60bd1c8f23ac38c9088ac64aa518037a3f58496
fatihgorguc <fatihgorguc@gmail.com>
2025-02-10 13:15:29 +0300

    github 404 test

--- Skill_System_Documentation/script.js
+++ Skill_System_Documentation/script.js
@@ -1,28 +1,64 @@
 const contentArea = document.getElementById('content-area');
 
-document.addEventListener("DOMContentLoaded", function () {
-    const params = new URLSearchParams(window.location.search);
-    const page = params.get("page");
-    const hash = window.location.hash;
+function loadContent(page) {
+    // Update iframe content
+    document.getElementById('content-frame').src = `pages/${page}`;
 
-    if (page) {
-        loadContent(page + hash);
-
-    } else {
-        loadContent("pages/Overview/Introduction.html");
-    }
-});
-function loadContent(htmlPath) {
-    fetch(htmlPath)
-        .then(response => response.text())
-        .then(html => {
-            contentArea.innerHTML = html;
-        })
-        .catch(error => {
-            console.error("Error loading content:", error);
-            contentArea.innerHTML = "Sorry, we couldn't load the content.";
-        });
+    // Update the browser URL without reloading the page
+    window.history.pushState({ page }, "", `/${page}`);
 }
+
+// Handle back/forward navigation
+window.onpopstate = function (event) {
+    if (event.state && event.state.page) {
+        document.getElementById('content-frame').src = `pages/${event.state.page}`;
+    }
+};
+
+// Load the correct page on first visit or refresh
+window.onload = function () {
+    const params = new URLSearchParams(window.location.search);
+    const page = params.get('page') || "Overview/Introduction.html";
+    loadContent(page);
+};
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const params = new URLSearchParams(window.location.search);
+//     const page = params.get("page");
+//     const hash = window.location.hash;
+//
+//     if (page) {
+//         loadContent(page + hash);
+//     } else {
+//         loadContent("pages/Overview/Introduction.html");
+//     }
+// });
+//
+//
+// window.onload = function () {
+//     const params = new URLSearchParams(window.location.search);
+//     const page = params.get('page');
+//     if (page) {
+//         loadContent(page);
+//     }
+// };
+//
+// function loadContent(htmlPath) {
+//     history.pushState(null, '', '?page=' + htmlPath);
+//     contentArea.src = htmlPath;
+//
+//     contentArea.onload = function () {
+//         const hash = htmlPath.split('#')[1];
+//         if (hash) {
+//             const doc = contentArea.contentDocument || contentArea.contentWindow.document;
+//             const targetElement = doc.getElementById(hash);
+//             if (targetElement) {
+//                 targetElement.scrollIntoView();
+//             }
+//         }
+//     };
+// }
+
 
 function toggleSubmenu(id) {
     let submenu = document.getElementById(id);
